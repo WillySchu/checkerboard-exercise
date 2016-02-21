a7a44c04e26f5d2cf4bb7efe517ddbf6f7f90613
Will Schumacher <meintodengel@gmail.com>
2016-02-21 08:38:54 -0800

    add solution

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your JS goes here
+var divs = [];
+var body = document.getElementsByTagName("body")[0];
+
+for (var i = 0; i < 63; i++) {
+  divs.push(document.createElement("div"));
+  divs[i].style.width = "11.1%";
+  divs[i].style.paddingBottom = "11.1%";
+  divs[i].style.float = "left";
+  if(i % 2 === 0) {
+    divs[i].style.backgroundColor = "black";
+  } else {
+    divs[i].style.backgroundColor = "red";
+  }
+  body.appendChild(divs[i]);
+}
