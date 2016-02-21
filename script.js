d567c868ab6956eb4f3942106743fe2abc0e3633
Will Schumacher <meintodengel@gmail.com>
2016-02-21 08:52:45 -0800

    add solution

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 var divs = [];
 var body = document.getElementsByTagName("body")[0];
 
+function getRandColor() {
+  var hex = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)]
+  }
+  return color;
+}
+
 for (var i = 0; i < 63; i++) {
   divs.push(document.createElement("div"));
   divs[i].style.width = "11.1%";
   divs[i].style.paddingBottom = "11.1%";
   divs[i].style.float = "left";
-  if(i % 2 === 0) {
-    divs[i].style.backgroundColor = "black";
-  } else {
-    divs[i].style.backgroundColor = "red";
-  }
+  divs[i].style.backgroundColor = getRandColor();
   body.appendChild(divs[i]);
 }
